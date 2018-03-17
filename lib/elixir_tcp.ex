059a788a2ec375ae15cb12ebbc1c1321924d361e
Orestis Markou <orestis@orestis.gr>
2018-03-17 11:02:19 +0100

    initial commit

---
+++ lib/elixir_tcp.ex
@@ -0,0 +1,18 @@
+defmodule ElixirTcp do
+  @moduledoc """
+  Documentation for ElixirTcp.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirTcp.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_tcp_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirTcpTest do
+  use ExUnit.Case
+  doctest ElixirTcp
+
+  test "greets the world" do
+    assert ElixirTcp.hello() == :world
+  end
+end
